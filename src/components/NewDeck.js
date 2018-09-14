cc3c55a2ddeba8a99b6a5e167f703500e3acdfc2
Ângelo Lucas <angelo.lrc@gmail.com>
2018-09-14 17:42:58 -0300

    New Deck title input

--- src/components/NewDeck.js
+++ src/components/NewDeck.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
+import styled from 'styled-components'
+
+const Title = styled.Text`
+  margin: 20px 10px 10px;
+  font-size: 18px;
+`
+const Input = styled.TextInput`
+  background-color: white;
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+`
 
 class NewDeck extends Component {
   render() {
-    return <View style={{ alignItems: 'center' }} />
+    return (
+      <View>
+        <Title>Deck Title</Title>
+        <Input />
+      </View>
+    )
   }
 }
 
