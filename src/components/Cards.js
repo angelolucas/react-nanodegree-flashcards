cd9b62db23243db7aca4d69cdb42263b4c3f6be2
Ângelo Lucas <angelo.lrc@gmail.com>
2018-09-17 15:22:58 -0300

    Card question and answer buttons

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
+import styled from 'styled-components'
 
 class Cards extends Component {
   render() {
     return (
-      <View>
-        <Text>Cards</Text>
+      <View style={{ flex: 1 }}>
+        <QuestionCard>
+          <Counter>1/10</Counter>
+          <Question>Dos React Native work with Android?</Question>
+        </QuestionCard>
+        <AnswerContainer>
+          <Answer>Incorrect</Answer>
+          <Answer>Correct</Answer>
+        </AnswerContainer>
       </View>
     )
   }
 }
+
+const QuestionCard = styled.View`
+  background: white;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+  flex: auto;
+  justify-content: space-around;
+`
+
+const Counter = styled.Text`
+  flex: 1;
+  text-align: right;
+`
+
+const Question = styled.Text`
+  font-size: 32px;
+  text-align: center;
+  flex: 1;
+`
+
+const AnswerContainer = styled.View`
+  flex-direction: row;
+  margin: 25px 0;
+`
+
+const Answer = styled.Text`
+  border-color: black;
+  border-width: 1px;
+  flex: 1;
+  margin: 10px;
+  text-align: center;
+  font-size: 28px;
+`
 
 export default Cards
