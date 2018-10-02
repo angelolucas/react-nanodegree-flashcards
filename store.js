8fbca202e62c4789207a710ed8ad4186ffa08287
Ângelo Lucas <angelo.lrc@gmail.com>
2018-10-02 17:33:33 -0300

    Redux devtools extension

--- store.js
+++ store.js
@@ -1,8 +1,9 @@
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from './reducer'
 import { AsyncStorage } from 'react-native'
 
-const store = createStore(reducer)
+const store = createStore(reducer, composeWithDevTools())
 
 store.subscribe = () => {
   AsyncStorage.setItem('store', JSON.stringify(store.getState()))
