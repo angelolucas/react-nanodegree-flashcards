defeee678c871c63e0ea26cdb111ff67da4a6d93
Ângelo Lucas <angelo.lrc@gmail.com>
2018-11-13 21:55:52 -0200

    setLocalNotification method

---
+++ utils/notifications.js
@@ -0,0 +1,42 @@
+const NOTIFICATION_KEY = 'UdaciFitness:notifications'
+
+export function setLocalNotification() {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then(data => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS).then(({ status }) => {
+          if (status === 'granted') {
+            Notifications.cancelAllScheduledNotificationsAsync()
+
+            let tomorrow = new Date()
+            tomorrow.setDate(tomorrow.getDate() + 1)
+            tomorrow.setHours(20)
+            tomorrow.setMinutes(0)
+
+            Notifications.scheduleLocalNotificationAsync(
+              {
+                title: 'Log your stats!',
+                body: "👋 don't forget to log your stats for today!",
+                ios: {
+                  sound: true,
+                },
+                android: {
+                  sound: true,
+                  priority: 'high',
+                  sticky: false,
+                  vibrate: true,
+                },
+              },
+              {
+                time: tomorrow,
+                repeat: 'day',
+              }
+            )
+
+            AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+          }
+        })
+      }
+    })
+}
